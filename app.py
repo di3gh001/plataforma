d4a68c6c86c189f771c6044c021747613aa6a009
Diego <conchatuex@gmail.com>
2023-11-11 10:00:10 -0400

    base de datos

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = "diego"
 app.config['MYSQL_HOST'] = 'Di3gh003.mysql.pythonanywhere-services.com'
 app.config['MYSQL_USER'] = 'Di3gh003'
 app.config['MYSQL_PASSWORD'] = '6482865Cbbab'
-app.config['MYSQL_DB'] = 'plataforma'
+app.config['MYSQL_DB'] = 'Di3gh003$plataforma'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
